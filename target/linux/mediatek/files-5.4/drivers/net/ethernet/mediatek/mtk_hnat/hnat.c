f3b9ae5f79d64ec52422ad3a6ff38a677ccb4e54
padavanonly <83120842+padavanonly@users.noreply.github.com>
2024-10-14 15:12:57 +0800

    Update hnat.c

--- target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat/hnat.c
+++ target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat/hnat.c
@@ -709,10 +709,7 @@ static int hnat_probe(struct platform_device *pdev)
 	dev_info(&pdev->dev, "lan = %s\n", hnat_priv->lan);
 
 	err = of_property_read_string(np, "mtketh-ppd", &name);
-	if (err < 0)
-		strncpy(hnat_priv->ppd, "wan", IFNAMSIZ);
-	else
-		strncpy(hnat_priv->ppd, (char *)name, IFNAMSIZ - 1);
+	strncpy(hnat_priv->ppd, "eth0", IFNAMSIZ);
 	dev_info(&pdev->dev, "ppd = %s\n", hnat_priv->ppd);
 
 	/*get total gmac num in hnat*/
